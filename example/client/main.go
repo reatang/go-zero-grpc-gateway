23e01cfe45dc16affaee5b9587e874d1ef479503
webhook_bot <tangtang1251@qq.com>
2023-12-06 16:33:58 +0800

    优化示例

--- example/client/main.go
+++ example/client/main.go
@@ -2,20 +2,16 @@ package main
 
 import (
 	"bytes"
-	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
-
-	"github.com/reatang/go-zero-grpc-gateway/example/simple_rpc/pb/simple_rpc"
 )
 
 var host = "http://127.0.0.1:8888"
 
 func main() {
-	req := simple_rpc.Request{Ping: "123"}
-	body, _ := json.Marshal(req)
+	body := []byte(`{"ping":"123"}`)
 
 	response, err := http.Post(host+"/grpc1/simple/ping", "application/json", bytes.NewReader(body))
 	if err != nil {
